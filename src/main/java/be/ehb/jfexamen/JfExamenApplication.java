56ca0f266f3e6d3fcd9afb190deb0e90ae86c01d
Mat. Devski Carlier <carliermateo@outlook.be>
2023-06-21 09:52:55 +0200

    Project initialisation

---
+++ src/main/java/be/ehb/jfexamen/JfExamenApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.jfexamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfExamenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/ehb/jfexamen/JfExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.jfexamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JfExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
