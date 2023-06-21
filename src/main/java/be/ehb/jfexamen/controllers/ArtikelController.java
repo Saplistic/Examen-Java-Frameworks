8e9d0d9ab479d6c27b7e8e53bffc14409cfdc78d
Mat. Devski Carlier <carliermateo@outlook.be>
2023-06-21 10:36:42 +0200

    Added a update artikel method

--- src/main/java/be/ehb/jfexamen/controllers/ArtikelController.java
+++ src/main/java/be/ehb/jfexamen/controllers/ArtikelController.java
@@ -3,9 +3,10 @@ package be.ehb.jfexamen.controllers;
 import be.ehb.jfexamen.model.DAO.ArtikelDAO;
 import be.ehb.jfexamen.model.entities.Artikel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RequestMapping("/api")
 @RestController
@@ -21,5 +22,21 @@ public class ArtikelController {
     @GetMapping("/artikels")
     public Iterable<Artikel> getAllArtikels() {
         return mArtikelDAO.findAll();
+    }
+
+    @PutMapping("/artikels")
+    private HttpStatus updateArtikel(@RequestParam int id,
+                                     @RequestParam String titel,
+                                     @RequestParam String inhoud) {
+
+        if (!mArtikelDAO.existsById(id)) {
+            return HttpStatus.NOT_FOUND;
+        }
+
+        Artikel artikelToUpdate = mArtikelDAO.findById(id).get();
+        artikelToUpdate.setTitel(titel);
+        artikelToUpdate.setInhoud(inhoud);
+        mArtikelDAO.save(artikelToUpdate);
+        return HttpStatus.OK;
     }
 }
